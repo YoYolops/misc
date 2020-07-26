00c085e90d77ad8bd14d796533ef0eb2e5c987e6
YoYolops <yohanhxh@gmail.com>
2020-07-26 03:55:33 -0300

    ainda desenvolvendo

--- solucionadorSudoku.R
+++ solucionadorSudoku.R
@@ -34,7 +34,7 @@ construtoSudoku <- construcaoSudoku() #a função gera o objeto construtoSudoku
 
 #indices dos núcleos:
 
-# 1  2  3     4  5  6     7  8  9
+#  1  2  3     4  5  6     7  8  9
 # 10 11 12    13 14 15    16 17 18
 # 19 20 21    22 23 24    25 26 27
 # 
@@ -46,9 +46,22 @@ construtoSudoku <- construcaoSudoku() #a função gera o objeto construtoSudoku
 # 64 65 66    67 68 69    70 71 72
 # 73 74 75    76 77 78    79 80 81
 
+indiceNucleo1 <- c(1,2,3,10,11,12,19,20,21)
+indiceNucleo2 <- c(4,5,6,13,14,15,22,23,24)
+indiceNucleo3 <- c(7,8,9,16,17,18,25,26,27)
+indiceNucleo4 <- c(28,29,30,37,38,39,46,47,48)
+indiceNucleo5 <- c(31,32,33,40,41,42,49,50,51)
+indiceNucleo6 <- c(34,35,36,43,44,45,52,53,54)
+indiceNucleo7 <- c(55,56,57,64,65,66,73,74,75)
+indiceNucleo8 <- c(58,59,60,67,68,69,76,77,78)
+indiceNucleo9 <- c(61,62,63,70,71,72,79,80,81)
 
 
-
+for(i in seq(1, length(construtoSudoku))){
+  if(construtoSudoku[i] == 0){ #as posições em brancos são representadas por 0
+    construtoSudoku[i] <- construtoSudoku[i] + 1
+  }
+}
 
 
   
