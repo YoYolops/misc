b0e23aa6905a4122c47caf133779406ecad7fa03
YoYolops <yohanhxh@gmail.com>
2020-11-22 18:43:15 -0300

    removendo testes

--- conversorBases.py
+++ conversorBases.py
@@ -1,4 +1,3 @@
-
 #função que inverte a lista para apresentar o resultado em ordem correta
 def inversor(lista):
     resposta = []
@@ -10,213 +9,111 @@ def inversor(lista):
 
     return resposta
 
+# Transforma valores maiores que dez em algarismos doaalfabeto
+def simbola(lista, tip):
+    base = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-# Conversor de Decimal para Binário:
-def DecBin(decimal):
-    resultado = []
+    if tip == 'cod':
+        for i in range(len(lista)):
+            if lista[i] > 9:
+                lista[i] = base[ lista[i]-10 ]
 
-    while decimal > 1:
-        add = decimal % 2
-        resultado.append(add)
-        decimal = int(decimal/2)
+    elif tip == 'decod':
+        for i in range(len(lista)):
+            if lista[i] in base:
+                lista[i] = base.index(lista[i]) + 10
 
-    resultado.append(1)
-    saida = inversor(resultado)
+    return lista
 
-    print('----BINÁRIO:----')
-    print(saida)
-    return saida
-
-# Conversor de Decimal para Octal: (usa conversão nativa do python)
-def DecOct(decimal):
-    print('OCTAL: %o'%decimal)
-
-    resultado = []
-
-    while decimal > 7:
-        add = decimal % 8
-        resultado.append(add)
-        decimal = int(decimal/8)
-
-    resultado.append(decimal)
-    saida = inversor(resultado)
-
-    return saida
-
-# Conversor de Decimal para Octal: (usa conversão nativa do python)
-def DecHex(decimal):
-    print('HEXADECIMAL: %x'%decimal)
-
-    resultado = []
-
-    while decimal > 15:
-        add = decimal % 16
-        resultado.append(add)
-        decimal = int(decimal/16)
-
-    resultado.append(decimal)
-    saida = inversor(resultado)
-
-    for i in range(len(saida)):
-        if saida[i] > 9:
-            if saida[i] == 10:
-                saida[i] = 'A'
-            elif saida[i] == 11:
-                saida[i] = 'B'
-            elif saida[i] == 12:
-                saida[i] = 'C'
-            elif saida[i] == 13:
-                saida[i] = 'D'
-            elif saida[i] == 14:
-                saida[i] = 'E'
-            elif saida[i] == 15:
-                saida[i] = 'F'
-
-    return saida
-
-def BinDec(bin):
+# Transforma de qualquer base para decimal
+def pDec(num, bEntrada):
     manipulacao = []
-    stringbin = str(bin)
+    stringNum = str(num)
 
-    for i in stringbin:
-        manipulacao.append(int(i))
+    if bEntrada > 10:
+        temp = simbola(list(stringNum), 'decod')
+        for i in temp:
+            manipulacao.append(int(i))
+    else:
+        for i in stringNum:
+            manipulacao.append(int(i))
 
     ctrl = len(manipulacao) - 1
     pot = 0
     while ctrl >= 0:
-        manipulacao[ctrl] = manipulacao[ctrl] * (2**pot)
+        manipulacao[ctrl] = manipulacao[ctrl] * (bEntrada**pot)
         pot += 1
         ctrl -= 1
-    
+
     saida = 0
     for i in manipulacao:
         saida = saida + i
 
-    print('----DECIMAL:----')
-    print(saida)
-    return saida
-        
-def OctDec(oct):
-    manipulacao = []
-
-    stringoct = str(oct)
-
-    for i in stringoct:
-        manipulacao.append(int(i))
-
-    ctrl = len(manipulacao) - 1
-    pot = 0
-    while ctrl >= 0:
-        manipulacao[ctrl] = manipulacao[ctrl] * (8**pot)
-        pot += 1
-        ctrl -= 1
-    
-    saida = 0
-    for i in manipulacao:
-        saida = saida + i
-    
-    print('----DECIMAL:----')
     print(saida)
     return saida
 
-def HexDec(hex):
-    manipulacao = []
+def dDec(decimal, bSaida):
+    resultado = []
+    decimal = int(decimal)
+    bSaida = int(bSaida)
 
-    stringhex = str(hex)
-
-    for i in stringhex:
-        manipulacao.append(i)
-
-    for i in range(len(manipulacao)):
-        if manipulacao[i] in ['a', 'A']:
-            manipulacao[i] = 10
-        elif manipulacao[i] in ['b', 'B']:
-            manipulacao[i] = 11
-        elif manipulacao[i] in ['c', 'C']:
-            manipulacao[i] = 12
-        elif manipulacao[i] in ['d', 'D']:
-            manipulacao[i] = 13
-        elif manipulacao[i] in ['e', 'E']:
-            manipulacao[i] = 14
-        elif manipulacao[i] in ['f', 'F']:
-            manipulacao[i] = 15
-        else:
-            manipulacao[i] = int(manipulacao[i])
-
-    ctrl = len(manipulacao) - 1
-    pot = 0
-    while ctrl >= 0:
-        manipulacao[ctrl] = manipulacao[ctrl] * (16**pot)
-        pot += 1
-        ctrl -= 1
+    while decimal > bSaida-1:
+        add = decimal % bSaida
+        resultado.append(add)
+        decimal = int(decimal/bSaida)
     
-    saida = 0
-    for i in manipulacao:
-        saida = saida + i
-    
-    print('----DECIMAL:----')
-    print(saida)
-    return saida
+    if bSaida == 2:
+        resultado.append(1)
+    else:
+        resultado.append(decimal)
 
-conversores = {
+    saida = simbola(inversor(resultado), 'cod')
 
-    'decimal':{
-        'binario': DecBin,
-        'octal': DecOct,
-        'hexadecimal': DecHex
-    },
+    print(f'Em base {bSaida}: {saida}')
+    print()
 
-    'binario':BinDec,
-    'octal':OctDec,
-    'hexadecimal':HexDec
-
+conversor = {
+    'para': pDec,
+    'de': dDec
 }
 
+
 print(
-    """ Conversor yoyo, v1.0.1.
+    """ Conversor yoyo, v2.0.0.
 
     INSTRUÇÕES:
 
-    Digite no terminal a base do número que será convertido,
-    por escrito, seguida da base do número para o qual se
-    quer converter, também por escrito.
+    Digite no terminal a base do número que será convertido (2,3,4,5 ... 35)
+    seguida da base do número para o qual se
+    quer converter.
 
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
     Exemplo:
 
     Para converter um numero de decimal para binário digite:
-    > $ decimal binario
+    > $ 10 2
     
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
     
-    BASES SUPORTADAS:
-
-    - Binária
-    - Octal
-    - Decimal
-    - Hexadecimal
+    Conversões de quaisquer bases entre 2 e 35
 
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 
 """
 )
 
+
 while True:
-    print('insira as bases de partida e chegada: ')
+    print()
+    print('insira, respectivamente, as bases de partida e de chegada: ')
     partida, chegada = input().split()
+    print()
 
+    if partida != '10' and chegada == '10':
+        conversor['para'](input('Insira o número a ser transformado: '), int(partida))
 
-    if partida != 'decimal' and chegada == 'decimal':
-        conversores[partida](input('Insira o número a ser transformado: '))
-
-    elif partida != 'decimal' and chegada != 'decimal':
-        a = conversores[partida](input('Insira o número a ser transformado: '))
-        conversores['decimal'][chegada](a)
-
+    elif partida != '10' and chegada != '10':
+        a = conversor['para'](input('Insira o número a ser transformado: '), 10)
+        conversor['de'](int(a), int(chegada))
     else:
-        conversores[partida][chegada](int(input('Insira o número a ser transformado: ')))
-
-
-
-    
-
+        conversor['de'](input('Insira o número a ser transformado: '), int(chegada))
